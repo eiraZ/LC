74d87f35045391819332e976bfbc4dc691f1d5ec
eiraZ <rzhang.umass@gmail.com>
2015-02-15 22:09:58 -0800

    Update N-Queens.java

--- N-Queens.java
+++ N-Queens.java
@@ -18,13 +18,13 @@ There exist two distinct solutions to the 4-queens puzzle:
   ".Q.."]
 ]
 
-Solution: DFS+ backtracking. 
+Solution: (1)DFS+ backtracking. 
           check row by row ->check availability -> if valid, mark ->next row
                                                     not valid, back to previous level
+          (2) bit manipulation.
 */
 
     public List<String[]> solveNQueens(int n) {
-    
         List<String[]> res = new ArrayList<String[]>();
         if (n <= 0) return res;
         int[] occupied = new int[n];
@@ -59,6 +59,47 @@ Solution: DFS+ backtracking.
             StringBuilder sb = new StringBuilder();
             for (int j =0; j < occupied.length; j++){
                 char c = (occupied[i] == j? 'Q': '.');
+                sb.append(c);
+            }
+            sol[i] = sb.toString();
+        }
+        return sol;
+    }
+    
+    public List<String[]> solveNQueens_bit(int n) {
+        List<String[]> res = new ArrayList<String[]>();
+        if (n <= 0) return res;
+        List<Integer> rec = new ArrayList<Integer>();
+        bitHelper(res, n, rec, 0, 0, 0);
+        return res;
+    }
+    
+    private void bitHelper(List<String[]> res, int n, List<Integer> rec, int row, int leftDi, int rightDi){
+        if (row == (1<<n) - 1){
+            String[] sol = print(rec);
+            res.add(sol);
+            return;
+        }
+        int mask = (1<<n) -1;
+        int avail = mask & (~( row | leftDi | rightDi));
+        while (avail != 0){
+            int pos = avail & (-avail);
+            rec.add(pos);
+            avail -= pos;
+            bitHelper(res, n, rec, row|pos, (leftDi|pos)<<1, (rightDi|pos)>>1);
+            rec.remove(rec.size() - 1);
+        }
+        
+    }
+    
+    private String[] print(List<Integer> rec){
+        String[] sol = new String[rec.size()];
+        for(int i = 0; i < sol.length; i++){
+            StringBuilder sb = new StringBuilder();
+            int cur = rec.get(i);
+            for (int j = 0; j < sol.length; j++){
+                int bit = (cur>>j) & 1;
+                char c = (bit==1? 'Q': '.');
                 sb.append(c);
             }
             sol[i] = sb.toString();
