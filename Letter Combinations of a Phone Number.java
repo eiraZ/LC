e63a1eaf4f871c238759251498fa4e6fbe079161
eiraZ <rzhang.umass@gmail.com>
2015-03-25 14:21:41 -0700

    Update Letter Combinations of a Phone Number.java

--- Letter Combinations of a Phone Number.java
+++ Letter Combinations of a Phone Number.java
@@ -5,6 +5,8 @@ A mapping of digit to letters (just like on the telephone buttons) is given belo
 
 Input:Digit string "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Solution: 1. DFS. digits.length() == m. every digit contains 4 chars at most. Time: O(4^m)
+          2. Memorization. from back to the front. Time: O(4^m).
 */
 public List<String> letterCombinations(String digits) {
     List<String> res = new ArrayList<String>();
@@ -29,4 +31,34 @@ private void helper(List<String> res, StringBuilder sb, String[] mem, String dig
         helper(res, sb, mem, digits, step+1);
         sb.deleteCharAt(sb.length() - 1);
     }
+}
+
+public List<String> letterCombinationsMemo(String digits) {
+        List<String> res = new ArrayList<String>();
+        if (digits.length() == 0)   return res;
+        
+        String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        Map<Integer, List<String>> combinations = new HashMap<>();
+        //from back to the front
+        int len = digits.length();
+        for (int i = len-1; i>=0; i--){
+            char c = digits.charAt(i);
+            String cmb = map[c-'0'];
+            List<String> ith = new ArrayList<>();
+            List<String> next = combinations.get(i+1);
+            for (int j = 0; j < cmb.length(); j++){
+                if(next == null){
+                    String sub = "" + cmb.charAt(j);
+                    ith.add(sub);
+                    continue;
+                }
+                for(String nextStr: next){
+                    String sub = "" + cmb.charAt(j) + nextStr;
+                    ith.add(sub);
+                }
+                
+            }
+            combinations.put(i, ith);
+        }
+        return combinations.get(0);
 }
