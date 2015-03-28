206bee0422bb6eef9c3e78bea1f460bb97d484c4
eiraZ <rzhang.umass@gmail.com>
2015-03-28 10:20:57 -0700

    Update Search in Rotated Sorted Array.java

--- Search in Rotated Sorted Array.java
+++ Search in Rotated Sorted Array.java
@@ -6,6 +6,7 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 You are given a target value to search. If found in the array return its index, otherwise return -1.
 
 You may assume no duplicate exists in the array.
+update range so that target is in this range.
 
 */
 public int search(int[] A, int target) {
