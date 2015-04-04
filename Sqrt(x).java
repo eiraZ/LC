700d78978c7700a1e9e666725974283cde87a2b0
eiraZ <rzhang.umass@gmail.com>
2015-04-04 15:48:56 -0700

    Update Sqrt(x).java

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -2,7 +2,11 @@
 Implement int sqrt(int x).
 Compute and return the square root of x.
 
-Solution: sqrt(x) < x/2+1. Binary search
+Solution: (1) sqrt(x) < x/2+1. Binary search
+          (2) Newton method. f(x) = x^2 - n. x^2 - n == 0. f'(x) = 2x.
+                f(x) - f(xi) = 2xi * (x-xi) --> f(x) = f(xi) + 2xi * (x-xi) == 0. f(xi) = xi^2 - n.
+                x = (xi + n/xi)/2.
+
 */
 public int sqrt(int x) {
     if (x < 0) return -1;
@@ -20,4 +24,17 @@ public int sqrt(int x) {
         }
     }
     return right;
+}
+
+public int sqrtNewton(int x) {
+    if (x < 0) return -1;
+    if (x == 0) return 0;
+    
+    double last = 0;
+    double res = 1;
+    while (res != last){
+        last = res;
+        res = (res + n/res)/2;
+    }
+    return (int)res;
 }
