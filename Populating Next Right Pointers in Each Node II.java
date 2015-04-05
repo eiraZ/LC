04ea007a96668b4e980824034b386682aedecd0d
eiraZ <rzhang.umass@gmail.com>
2015-04-04 18:12:44 -0700

    Update Populating Next Right Pointers in Each Node II.java

--- Populating Next Right Pointers in Each Node II.java
+++ Populating Next Right Pointers in Each Node II.java
@@ -20,7 +20,8 @@ After calling your function, the tree should look like:
      / \    \
     4-> 5 -> 7 -> NULL
     
-    Solution: BFS+ recursion. treat it as a linkedlist.
+    Solution: 1. BFS+ recursion. treat it as a linkedlist.
+              2. DFS. List<List<TreeLinkNode>> res. record last TreeLinkNode for each level.
 */
 public void connect(TreeLinkNode root) {
         if(root==null){
@@ -43,3 +44,27 @@ public void connect(TreeLinkNode root) {
         }
         connect(dummy.next);
   }
+  
+      public void connect(TreeLinkNode root) {
+        //DFS
+        if(root == null)    return;
+        List<List<TreeLinkNode>> res = new ArrayList<>();
+        helper(res, root, 0);
+        
+    }
+   private void helper(List<List<TreeLinkNode>> res, TreeLinkNode root, int level){
+        if (root == null)   return;
+        List<TreeLinkNode> ele = null;
+        if (res.size() == level){
+            ele = new ArrayList<TreeLinkNode>();
+            ele.add(root);
+            res.add(ele);
+        }else{
+            ele = res.get(level);
+            ele.get(0).next = root;
+            ele.remove(0);
+            ele.add(root);
+        }
+        helper(res, root.left, level + 1);
+        helper(res, root.right, level + 1);
+    }
